71bbd8f451094c0072ee512a85c73a1b459853e0
Guma <lauralp21.love@gmail.com>
2026-06-07 19:53:27 -0300

    Hecho Hasta Urban

--- Hobb/index.js
+++ Hobb/index.js
@@ -1,3 +1,31 @@
+if (window.location.pathname.includes("index.html")) {
+    function showMusicWarning() {
+        const warning =
+            document.createElement("div");
+        warning.className = "music-warning";
+        warning.textContent = "♫ Music incoming!!";
+        document.body.appendChild(
+            warning);
+        setTimeout(
+            () => warning.remove(), 10000
+        );
+    };
+    showMusicWarning()
+}
+if (window.location.pathname.includes("index2.html")) {
+    function showMusicWarning() {
+        const warning =
+            document.createElement("div");
+        warning.className = "music-warning";
+        warning.textContent = "♫ HI AGAIN!!";
+        document.body.appendChild(
+            warning);
+        setTimeout(
+            () => warning.remove(), 10000
+        );
+    }
+    showMusicWarning();
+}
 //! seccion para mirar imagenes mas grandes
 const images = document.querySelectorAll(".intro--img");
 const overlay = document.getElementById("overlay");
@@ -47,7 +75,20 @@ document.querySelectorAll(".copy-text").forEach(el => {
 //! Musica
 const tracks = document.querySelectorAll(".track");
 const player = document.getElementById("music--player");
+//! volumen slider control
+const volumeSlider =
+    document.getElementById(
+        "volume-slider"
+    );
 
+volumeSlider.addEventListener(
+    "input",
+    () => {
+
+        player.volume =
+            volumeSlider.value / 100;
+
+    });
 let currentTrack = null;
 
 tracks.forEach(track => {
@@ -56,17 +97,24 @@ tracks.forEach(track => {
         const img = track.querySelector("img");
         if (img) {
             cursor.style.backgroundImage = `url('${img.src}')`;
+            volume_ball.style.setProperty(
+                "--thumb-image",
+                `url('${img.src}')`
+            );
+
         }
 
         if (currentTrack == track) {
 
             if (!player.paused) {
+                volumeSlider.classList.remove("rotating");
                 player.pause();
                 const img = track.querySelector("img");
                 img.src = track.dataset.static;
                 track.classList.remove("active");
             } else {
                 player.play();
+                volumeSlider.classList.add("rotating");
                 const img = track.querySelector("img");
                 track.classList.add("active");
             }
@@ -81,16 +129,117 @@ tracks.forEach(track => {
             currentTrack.classList.remove("active");
         }
 
+        setupVisualizer();
         player.src = track.dataset.song;
-        player.volume = .5;
+        songName.textContent =
+            track.dataset.song
+                .split("/")
+                .pop()
+                .replace("mp3", "");
+        player.volume = .2;
         player.play();
 
 
         track.classList.add("active");
-
+        volumeSlider.classList.add("rotating");
         currentTrack = track;
     });
 });
+//! INICIO CONTROLADOR MUSICA
+const progress =
+    document.getElementById(
+        "progress"
+    );
+const songName =
+    document.getElementById(
+        "song-name"
+    );
+const canvas =
+    document.getElementById(
+        "visualizer"
+    );
+canvas.width =
+    canvas.offsetWidth;
+canvas.height =
+    canvas.offsetHeight;
+const ctx =
+    canvas.getContext(
+        "2d"
+    );
+
+let audioContext;
+let analyser;
+let dataArray;
+let bufferLength;
+
+function setupVisualizer() {
+    if (audioContext) return;
+    audioContext = new AudioContext();
+    const source = audioContext.createMediaElementSource(player);
+    analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 256;
+    bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+}
+
+function bounce() {
+
+    requestAnimationFrame(
+        bounce
+    );
+    if (!analyser)
+        return;
+    analyser.getByteFrequencyData(
+        dataArray
+    );
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+    let barWidth =
+        canvas.width /
+        bufferLength;
+    for (
+        let i = 0;
+        i < bufferLength;
+        i++
+    ) {
+        const smoothData =
+            new Array(
+                bufferLength
+            ).fill(0);
+        smoothData[i] +=
+            (dataArray[i] - smoothData[i]) * 0.2;
+        let height = smoothData[i];
+        ctx.fillStyle = `hsl(${height},100%,50%)`;
+        ctx.fillRect(
+            i * barWidth,
+            canvas.height - height,
+            barWidth - 2,
+            height
+        );
+    }
+}
+bounce();
+player.addEventListener(
+    "timeupdate",
+    () => {
+        if (player.duration) {
+            progress.value = (player.currentTime / player.duration) * 100;
+        }
+    }
+);
+progress.addEventListener(
+    "input",
+    () => {
+        player.currentTime = (progress.value / 100) * player.duration;
+    }
+);
+//!FIN CONTROLADOR MUSICA
 player.addEventListener("ended", () => {
     if (currentTrack) {
         const img = currentTrack.querySelector("img");
@@ -98,15 +247,20 @@ player.addEventListener("ended", () => {
         img.src = currentTrack.dataset.static;
 
         currentTrack.classList.remove("active");
+        volumeSlider.classList.remove("rotating");
+        progress.value = 0;
+
+        songName.textContent =
+            "No song";
         currentTrack = null;
     }
 });
 const playlist = document.querySelector(".playlist");
 const toggleBtn = document.getElementById("togglePlaylist");
-
+const volumeBox = document.querySelector(".volume--container");
 toggleBtn.addEventListener("click", () => {
     playlist.classList.toggle("open");
-
+    volumeBox.classList.toggle("open");
     toggleBtn.textContent =
         playlist.classList.contains("open")
             ? "[X]"
@@ -114,6 +268,8 @@ toggleBtn.addEventListener("click", () => {
 });
 //! cursor
 const cursor = document.createElement("div");
+//! slider volume
+const volume_ball = document.querySelector(".volume--slider-input");
 cursor.classList.add("cursor");
 document.body.appendChild(cursor);
 
@@ -141,15 +297,51 @@ function animate() {
 animate();
 
 //! Swiper
+//! Abajo CHICO
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
 });
+//! GRANDE
 var swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     thumbs: {
         swiper: swiper,
     },
 });
+//! GRID
+var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    grid: {
+        rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "fraction" //bullets - fraction - progressbar
+    }, scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+        direction: "horizontal",
+    },
+    mousewheel: true,
+    //effect:fade - slide - cube - cards - coverflows
+    //centeredSlides: true,
+    /*breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }*/
+});
+
